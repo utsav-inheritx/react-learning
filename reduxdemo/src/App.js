813c61df4d1fcf4b537ac12b5676867e72b929c1
utsav-inheritx <utsav.k@inheritx.com>
2024-07-24 12:46:42 +0530

    first commit

---
+++ reduxdemo/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './redux/slices/counter'
+
+function App() {
+
+  const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch()
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Count is {count}</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
