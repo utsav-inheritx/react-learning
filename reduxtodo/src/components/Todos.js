b57b394f23fb433edf08f8670799fb1fdcdbb878
utsav-inheritx <utsav.k@inheritx.com>
2024-07-26 12:55:54 +0530

    redux todo

--- reduxtodo/src/components/Todos.js
+++ reduxtodo/src/components/Todos.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTodo } from '../slices/todo/todoSlice';
 
+
 const Todos = () => {
     const todos = useSelector((state) => state.todos);
     const dispatch = useDispatch();
